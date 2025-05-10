c11d5bf1d1546e0164b010ae4a51e8908dcbf6c9
Deepak <deepakviji5556@gmail.com>
2025-05-10 22:12:20 +0530

    Learn UI

---
+++ lib/common/custom.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Widget custom() {
+  return Text("Let's Learn Together");
+}
+
+Widget textVariable({required String TextVal}) {
+  return Text(TextVal);
+}
